80709f08e706e11da187b11264df1cd4caee8f8b
Maxwell Golo <maxwellgol123@gmail.com>
2025-09-17 05:09:31 +0000

    Add 2-arguments.js to print message based on argument count

---
+++ 2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const argCount = process.argv.length - 2;
+
+if (argCount === 0) {
+  console.log('No argument');
+} else if (argCount === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
